3b228c6dff76b48848509231148e68aa0ba8f894
Gudala Sambasiva <119730311+sambasivagudala369@users.noreply.github.com>
2025-02-27 14:16:49 +0530

    Time: 0 ms (100%), Space: 12.5 MB (8.99%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isArraySpecial(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        for i in range(0,(len(nums)-1)):
+            if(nums[i]%2==nums[i+1]%2):
+                return False
+        return True         
+
+        
