a0ed3135ec296fdbc4e0a03d01cab724b4ec1138
Gudala Sambasiva <119730311+sambasivagudala369@users.noreply.github.com>
2025-02-27 14:59:53 +0530

    Time: 55 ms (15.96%), Space: 13.2 MB (96.99%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -7,7 +7,7 @@ class Solution(object):
         i=1
         while i<len(nums):
             if nums[i] == nums[i-1]:
-                nums.pop(i) #using del is the best option instead of using pop
+                del nums[i] #using del is the best option instead of using pop
             else:
                 i+=1
         return len(nums)          
