7eff6eff9f379cf4ac235eca929c13735cee3917
Gudala Sambasiva <119730311+sambasivagudala369@users.noreply.github.com>
2025-03-16 19:52:27 +0530

    Time: 1 ms (100%), Space: 45.7 MB (28.61%) - LeetHub

---
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n = grid.length;
+        boolean[] visited = new boolean[n * n + 1];
+        int repeatedValue = -1;
+        int xorWithoutAB = 0; 
+        int xorOfAll = 0;
+        int cnt = 1;
+
+        for (var row : grid) {
+            for (var element : row) {
+                xorWithoutAB ^= element;
+                xorOfAll ^= cnt;
+                cnt += 1;
+                if (repeatedValue == -1) {
+                    if (visited[element]) {
+                        repeatedValue = element;
+                    } else {
+                        visited[element] = true;
+                    }
+                }
+            }
+        }
+        int missingValue = xorWithoutAB ^ xorOfAll ^ repeatedValue;
+        return new int[] {repeatedValue, missingValue};
+    }
+}
