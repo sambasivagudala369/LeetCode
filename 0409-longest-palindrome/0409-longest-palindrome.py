44536ea2d64ecdc308a23ecfec2e1b14b819954c
Gudala Sambasiva <119730311+sambasivagudala369@users.noreply.github.com>
2025-06-12 22:21:34 +0530

    Time: 2 ms (50.94%), Space: 18 MB (27.46%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        freq = Counter(s)
+        length = 0
+        odd_found = False
+        for count in freq.values():
+            if count % 2 == 0:
+                length += count
+            else:
+                length += count - 1
+                odd_found = True
+        if odd_found:
+            length += 1
+        return length
