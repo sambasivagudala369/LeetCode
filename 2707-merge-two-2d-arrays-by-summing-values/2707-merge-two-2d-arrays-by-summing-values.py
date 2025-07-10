7b30e12fb1b2ac820bfa17dccb64ea640b65bd19
Gudala Sambasiva <119730311+sambasivagudala369@users.noreply.github.com>
2025-07-10 22:26:33 +0530

    Time: 1 ms (51.43%), Space: 12.6 MB (62.04%) - LeetHub

---
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def mergeArrays(self, nums1, nums2):
+        """
+        :type nums1: List[List[int]]
+        :type nums2: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        d ={}
+        for i, v in nums1:
+            d[i] = d.get(i, 0) + v
+        for i, v in nums2:
+            d[i] = d.get(i, 0) + v
+        return sorted(d.items())
+        
